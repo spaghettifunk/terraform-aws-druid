04d6cd2254213a2fa9cd5e6a3ff7b706971e3595
Davide Berdin <davideberdin@gmail.com>
2020-05-28 18:29:49 +0200

    postgres url

--- locals.tf
+++ locals.tf
@@ -1,5 +1,5 @@
 locals {
   druid_image      = "${format("%s/%s:%s", var.druid_image_registry, var.druid_image_repository, var.druid_image_tag)}"
-  postgres_url     = "${format("%s:%s@%s:%s/%s", var.postgres_user, var.postgres_password, var.postgres_host, var.postgres_port, var.postgres_db)}"
+  postgres_url     = var.postgres_host
   zookeeper_server = var.zookeeper_host
 }
